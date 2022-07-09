b435cf48e88ec792d0d93a03d2fc2a936c2b005a
AleksandrSherehkov <101018177+AleksandrSherehkov@users.noreply.github.com>
2022-07-09 22:30:04 +0000

    Deploying to gh-pages from @ AleksandrSherehkov/goit-js-hw-09@16580c06d7618343b099ff04fdfb5596553e8dd0 🚀

---
+++ 01-color-switcher.120d07df.js
@@ -0,0 +1,2 @@
+!function(){var t={body:document.querySelector("body"),startBtn:document.querySelector("button[data-start]"),stopBtn:document.querySelector("button[data-stop]")},e=null;t.startBtn.addEventListener("click",(function(n){e=setInterval((function(){t.body.style.backgroundColor="#".concat(Math.floor(16777215*Math.random()).toString(16))}),1e3),t.startBtn.setAttribute("disabled","disabled")})),t.stopBtn.addEventListener("click",(function(n){clearInterval(e),t.startBtn.removeAttribute("disabled")}))}();
+//# sourceMappingURL=01-color-switcher.120d07df.js.map
